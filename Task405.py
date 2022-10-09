fe73162b62538088113e2abef09b06f6ceaa146f
Dmitry <orionvega5@gmail.com>
2022-10-09 20:51:51 +0300

    Add Task405

---
+++ Task405.py
@@ -0,0 +1,81 @@
+# Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+
+# data1 = '74*X^5 + 28*X^4 + 46*X^3 + 57*X^2 + 30*X + 13'
+data1 = '74*X^5 + 28*X^4 + 46*X^3 + 57*X^2 + 13'
+# data1 = '74*X^5 + 28*X^4 + 46*X^3 + 57*X^2 + 30*X'
+data2 ='27*X^5 + 51*X^4 + 47*X^3 + 4*X^2 + 52*X + 3'
+
+def end_equation(list_koef, equation):
+        if list_koef[-2] != '0':
+            equation += list_koef[-2]+'*'+'X' + ' + ' + list_koef[-1]
+        else: equation += list_koef[-1]
+        return equation
+
+# path1 = 'file_equa1.txt'
+# path2 = 'file_equa2.txt'
+# f = open(path1, 'r')
+# data1 = f.read()
+# f.close()
+
+# f = open(path2, 'r')
+# data2 = f.read()
+# f.close()
+# print('Данные из файла', data1)
+# print('Данные из файла', data2)
+
+data_list1 = data1.split(' + ')
+data_list2 = data2.split(' + ')
+print('Fist eqation: ', data_list1)
+print('Second eqation:', data_list2)
+
+def split_data(data_list):
+    data_dic = {}
+    # data_dic1['1'] = []
+    for i in range(len(data_list)):
+        if '^' in data_list[i]:
+            stepen = data_list[i].split('^')[1] #Степень во втором элементе строки
+            data_dic[stepen] = data_list[i].split('*')[0]
+        elif (not '^' in data_list[i]) and 'X' in data_list[i] :
+            data_dic['1'] = data_list[i].split('*')[0]
+        else: data_dic['0'] = data_list[i]
+    return data_dic   
+
+data_dic1 = split_data(data_list1)
+print('First Dictionary', data_dic1)
+data_dic2 = split_data(data_list2)
+print('Second Dictionary', data_dic2)
+#Далее создание словоря
+dic_res = {}
+for key1 in data_dic1.keys(): #Вывод ключей
+    for key2 in data_dic2.keys():
+        # print(key2*4)
+        if key1 == key2:
+            dic_res[key1] = str(int(data_dic1[key1]) + int(data_dic2[key2]))
+        elif key1 in data_dic1.keys() and (not key1 in data_dic2.keys()):
+            print(key1)
+            dic_res[key1] = str(int(data_dic1[key1]))
+        # elif key2 in data_dic2.keys() and (not key2 in data_dic2.keys()):
+        #     dic_res[key2] = str(int(data_dic1[key2]))
+print('Final Dictionary', dic_res)
+
+k = int(list(dic_res.keys())[0])
+# print(k)
+list_koef = list(dic_res.values())
+# print(list_koef)
+
+equation = 'Y = '
+
+if k >= 2:
+    for i in range(len(list_koef)-2):
+        if list_koef[i] != '0': # Проверка на нулевой множитель
+            equation += list_koef[i] +'*'+'X'+'^'+ str(len(list_koef)-1-i) + ' + '
+    equation = end_equation(list_koef, equation) #Вызов функции окончания выражения
+elif k == 1: #Можно записать elif k:
+    equation = end_equation(list_koef, equation) #Вызов функции окончания выражения
+else: equation += list_koef[-1]
+
+print(equation)
+
+with open('file_equa_task405.txt', 'w') as my_data:
+    my_data.write(equation)   
